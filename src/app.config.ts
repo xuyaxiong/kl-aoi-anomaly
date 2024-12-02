cd3a03deabb1850576802744080f3c694b3534f9
xuyaxiong <xyxlindy@163.com>
2024-12-02 15:26:33 +0800

    添加aoiDB模块

--- src/app.config.ts
+++ src/app.config.ts
@@ -3,5 +3,18 @@ const AppConfig = {
   CRASH_DUMP_DIR: 'D:\\kl-storage\\crashDump\\aoi-anomaly', // dll崩溃dump文件保存路径
   APP_LOG_DIR: 'D:\\kl-storage\\app-logs\\aoi-anomaly', // 服务日志保存路径
   TMP_DIR: 'D:\\tmp', // 临时文件夹
+
+  imgInfo: {
+    width: 5120,
+    height: 5120,
+    channel: 3,
+  },
+
+  exportPath: {
+    appPath: 'D:\\kl-storage\\',
+    recipePath: 'D:\\kl-storage\\gallery\\recipe\\',
+    dbPath: 'D:\\kl-storage\\gallery\\db\\',
+    samplePath: 'D:\\kl-storage\\gallery\\sample\\',
+  },
 };
 export default AppConfig;
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnomalyController } from './anomaly/anomaly.controller';
 import { AnomalyService } from './anomaly/anomaly.service';
+import { AOIDBModule } from '@koala123/aoi-db';
+import AppConfig from './app.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [localConfig] })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [localConfig] }),
+    AOIDBModule.forRoot({
+      width: AppConfig.imgInfo.width,
+      height: AppConfig.imgInfo.height,
+      channel: AppConfig.imgInfo.channel,
+      dbPath: AppConfig.exportPath.dbPath,
+      dllPath: AppConfig.DLL_PATH,
+    }),
+  ],
   controllers: [AppController, AnomalyController],
   providers: [AppService, AnomalyService],
 })
