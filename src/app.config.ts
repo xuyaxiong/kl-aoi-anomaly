36141e91be44e53b991678d09679ff0f1911b2e8
xuyaxiong <xyxlindy@163.com>
2024-12-19 15:48:18 +0800

    修改配置文件

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,7 +1,11 @@
+const path = require('path');
+
+const KL_STORAGE_PATH = 'D:\\kl-storage';
+
 const AppConfig = {
-  DLL_PATH: 'D:\\kl-storage\\dll\\', // dll路径
-  CRASH_DUMP_DIR: 'D:\\kl-storage\\crashDump\\aoi-anomaly', // dll崩溃dump文件保存路径
-  APP_LOG_DIR: 'D:\\kl-storage\\app-logs\\aoi-anomaly', // 服务日志保存路径
+  DLL_PATH: path.join(KL_STORAGE_PATH, 'dll'), // dll路径
+  CRASH_DUMP_DIR: path.join(KL_STORAGE_PATH, 'crashDump', 'aoi-anomaly'), // dll崩溃dump文件保存路径
+  APP_LOG_DIR: path.join(KL_STORAGE_PATH, 'app-logs', 'aoi-anomaly'), // 服务日志保存路径
   TMP_DIR: 'D:\\tmp', // 临时文件夹
 
   imgInfo: {
@@ -11,10 +15,10 @@ const AppConfig = {
   },
 
   exportPath: {
-    appPath: 'D:\\kl-storage\\',
-    recipePath: 'D:\\kl-storage\\gallery\\recipe\\',
-    dbPath: 'D:\\kl-storage\\gallery\\db\\',
-    samplePath: 'D:\\kl-storage\\gallery\\sample\\',
+    appPath: `${KL_STORAGE_PATH}\\`,
+    recipePath: `${KL_STORAGE_PATH}\\gallery\\recipe\\`,
+    dbPath: `${KL_STORAGE_PATH}\\gallery\\db\\`,
+    samplePath: `${KL_STORAGE_PATH}\\gallery\\sample\\`,
   },
 
   registerHealthCheck: 'ws://127.0.0.1:9000?clientType=service',
